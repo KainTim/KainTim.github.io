b8ae1b6c8d4c97fddf57bda2d3285d6e97008f7b
Tim Kainz <kainzt21030@sus.htl-grieskirchen.at>
2025-06-07 15:56:42 +0200

    updated page

---
+++ _app/immutable/chunks/2HcqyP_M.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_6rhiky)==null?void 0:s.base)??"/kaintim.github.io";var t;const i=((t=globalThis.__sveltekit_6rhiky)==null?void 0:t.assets)??a;export{i as a,a as b};
